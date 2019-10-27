bd30385e5b07cb235b371716f0140f947b069b69
Isaivargas <Isaivargas@users.noreply.github.com>
2019-10-26 22:19:58 -0500

    Update admin.py

--- HackProyect/prueba/admin.py
+++ HackProyect/prueba/admin.py
@@ -1,4 +1,4 @@
-#from django.contrib import admin
+from django.contrib import admin
 
 
 from .models import Pregunta,Respuesta,Doctor,Articulo,Sintoma,Medicamento,Tratamiento,ArticuloSistema,TratamientoMedicamento
