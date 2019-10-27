432b08690f98f4359d1b53a08a5380c334456964
JUAN NOLASCO <jjuanccarloss_96@hotmail.com>
2019-10-27 10:54:30 -0600

    Front totales

--- HackProyect/doc-service/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Pregunta(models.Model):
-    descripcion = models.CharField(max_length=200)
-    texto = models.CharField(max_length=200)
-
